c63de190a59faa78561d88e7f96b5e2e61d2aa29
mikala-randich <randichmr@g.cofc.edu>
2016-04-19 16:11:49 -0400

    Delete densitymapscript.R

--- densitymapscript.R
+++
@@ -1,8 +0,0 @@
-densityMap <- function(dat, var){
-  varChart <- ggplot(data = dat, aes(x=Utm_E, y=Utm_N, z=var)) + 
-    geom_point(aes(color = var)) +
-    scale_color_gradient(low="yellow",high="red")+
-    stat_density2d(geom = "polygon", n=30, aes(fill=..level..))+
-    geom_point(aes(color = var))
-  return(varChart)
-}
